c9b3b237651ba0fb8d92997d292752c727967aa6
JOHN-LAPTOP\João <joaozoares@gmail.com>
2021-03-18 10:55:11 +0000

    Bugged first version

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // sc.nextInt();
+        int points = Integer.parseInt(args[0]);
+
+        int insideCounter = 0;
+
+        for (int i = 0; i < points; i++) {
+            double xCoord = Math.random();
+            double yCoord = Math.random();
+            double res = (xCoord * xCoord) + (yCoord * yCoord);
+            if (res <= 1) {
+                insideCounter++;
+            }
+        }
+
+        double ro = insideCounter / points;
+        double pi = ro * 4;
+        System.out.println("Total number of points: " + points);
+        System.out.println("Points within the circle: " + insideCounter);
+        System.out.println("Pi estimation: " + pi);
+
+        sc.close();
+    }
+}
